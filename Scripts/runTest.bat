c2a48e15d2d679aed2bae885bda6db0a2bf4cba3
MahBoiTranslator <mah.boi.translator@gmail.com>
2023-01-05 17:10:31 +0300

    Перевод зданий Скринов

--- Scripts/runTest.bat
+++ Scripts/runTest.bat
@@ -2,8 +2,8 @@
 
 : Сборка и копирование файлов локализации
 call BuildWithoutMaps.bat
-xcopy /h /y /c /r /s ..\Translation\TiberiumEssence2_Russian.big %USERPROFILE%\Documents\"Command & Conquer 3 Tiberium Wars"\Mods\TiberiumEssence2\TiberiumEssence2_Russian.big
-xcopy /h /y /c /r /s ..\Translation\TiberiumEssence_2.0.skudef   %USERPROFILE%\Documents\"Command & Conquer 3 Tiberium Wars"\Mods\TiberiumEssence2\TiberiumEssence_2.0.skudef
+xcopy /y /c ..\Translation\TiberiumEssence2_Russian.big %USERPROFILE%\Documents\"Command & Conquer 3 Tiberium Wars"\Mods\TiberiumEssence2\TiberiumEssence2_Russian.big
+xcopy /y /c ..\Translation\TiberiumEssence_2.0.skudef   %USERPROFILE%\Documents\"Command & Conquer 3 Tiberium Wars"\Mods\TiberiumEssence2\TiberiumEssence_2.0.skudef
 
 : Задержка необходима для того, чтобы файлы успели скопироваться
 timeout /t 3
